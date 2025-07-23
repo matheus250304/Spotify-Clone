7f9af9b88fbfe255afae0168bd302b4ac4521d8d
Matheus Matos <matheuscarmom@gmail.com>
2025-07-23 18:58:36 -0300

    Spotify-Clone_23/07/2025

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const artistData = [
+  { name: "Henrique & Juliano", image: "./img/artista-henrique-juliano.jpeg" },
+  { name: "Jorge & Mateus", image: "./img/artista-jorge-mateus.jpeg" },
+  { name: "Zé Neto & Cristiano", image: "./img/artista-ze-neto.jpeg" },
+  { name: "Gusttavo Lima", image: "./img/artista-gustavo-limma.jpeg" },
+  { name: "Luan Santana", image: "./img/artista-luan-santana.jpeg" },
+  { name: "Matheus & Kauan", image: "./img/artista-mateus-kauan.jpeg" },
+];
+const albunsData = [
+  {
+    name: "White Noise (Sleep & Relaxation Sounds)",
+    artist: "Sleeply John",
+    image: "./img/album-white-noise.jpeg",
+  },
+  {
+    name: "O Céu Explica Tudo(Ao vivo)",
+    artist: "Henrique & Juliano",
+    image: "./img/album-ceu-",
+  },
+  { name: "", artist: "", image: "" },
+  { name: "", artist: "", image: "" },
+  { name: "", artist: "", image: "" },
+  { name: "", artist: "", image: "" },
+];
